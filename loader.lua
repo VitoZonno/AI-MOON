af5ca924aefdb6210e76afe189136e35afc26855
Vitoooooo <vitozonno041@gmail.com>
2026-04-07 18:45:55 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,4 @@
+-- ESEGUI QUESTO NELL'EXECUTOR
+local user = "VitoZonno"
+local repo = "AI-MOON"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/"VitoZonno"/"AI-MOON"/main/Core/Main.lua"))()
